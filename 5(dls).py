ef3ba8506baefbf2b97f68e79b81a1fe1c66b388
aadyakatara <140581092+aadyakatara@users.noreply.github.com>
2024-01-27 22:49:24 +0530

    Create 5(dls).py

---
+++ 5(dls).py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class Graph:
+    def _init_(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def addNode(self, v, visited, depth_limit):
+        if depth_limit == 0:
+            return
+        visited.add(v)
+        print(v, end=' ')
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.addNode(neighbour, visited, depth_limit - 1)
+
+    def DFS(self, v, depth_limit):
+        visited = set()
+        self.addNode(v, visited, depth_limit)
+
+my_graph = Graph()
+v = int(input("Enter number of nodes: "))
+for _ in range(v - 1):
+    x = int(input("Enter x: "))
+    y = int(input("Enter y: "))
+    my_graph.addEdge(x, y)
+
+print("Depth-Limited Search:")
+start = int(input("Enter start: "))
+limit = int(input("Enter Limit: "))
+my_graph.DFS(start, limit)
